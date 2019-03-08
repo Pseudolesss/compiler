79f6b1b06d7592dc96d8efbb4fc78541450690d5
minh <minh.schils@student.uliege.be>
2019-03-08 13:37:48 +0100

    header

--- ASTnode.h
+++ ASTnode.h
@@ -68,12 +68,154 @@ struct Field : ASTnode
     public:
         Field(String,Type);
         Field(String,Type,Expr);
-        getID();
-        getType();
-        getExpr();
+        String getID();
+        Type getType();
+        Expr getExpr();
 
     private:
         String objID;
         Type type;
         Expr expr;
-}
+};
+
+struct Method : ASTnode
+{
+    public:
+        Method(String, Formals,Type, Block);
+        String getID();
+        Formals getFormals();
+        Type getType();
+        Block getBlock();
+    private:
+        String objID;
+        Formals formals;
+        Type type;
+        Block block;
+};
+
+struct Type : ASTnode
+{
+    public:
+        Type(String);
+        Type getType();
+    private:
+        String Type;
+};
+
+struct Formals : ASTnode
+{
+    public:
+        Formals()
+        Formals(Formal, Formalx)
+        Formal getFormal();
+        Formalx getFormalx();
+    private:
+        Formal formal;
+        Formalx formalx;
+};
+
+struct Formalx : ASTnode
+{
+    public:
+        Formalx();
+        Formalx(Formal,Formalx);
+        Formal getFormal();
+        Formalx getFormalx();
+    private:
+        Formal formal;
+        Formalx formalx;
+};
+
+struct Formal : ASTnode
+{
+    public:
+        Formal(String,Type);
+        String getID();
+        Type getType();
+    private:
+        String objID;
+        Type type;
+};
+
+struct Block : ASTnode
+{
+    public:
+        Block (Expr,Exprx);
+        Expr getExpr();
+        Exprx getExprx();
+    private:
+        Expr expr;
+        Exprx exprx;
+};
+
+struct Expr : ASTnode
+{ //nothing to do.... i think
+};
+struct If : Expr
+{
+    public:
+        If(Expr,Expr,Expr);
+        If(Expr,Expr);
+        Expr getIf();
+        Expr getThen();
+        Expr getElse();
+    private:
+        Expr _if;
+        Expr _then;
+        Expr _else;
+};
+struct While : Expr
+{
+    public:
+        While(Expr,Expr);
+        Expr getWhile();
+        Expr getDo();
+    private:
+        Expr _while;
+        Expr _do;
+};
+struct Let : Expr
+{
+    public:
+        Let(String,Type,Expr);
+        Let(String,Type,Expr,Expr);
+        String getObjID();
+        Type getType();
+        Expr getAssign();
+        Expr getIn();
+    private: 
+        String ObjID;
+        Type type;
+        Expr assign;
+        Expr in;
+};
+struct Assign : Expr
+{
+    public:
+        Assign(String,Expr);
+        String getObjID();
+        Expr getExpr();
+    private:
+        String ObjID;
+        Expr expr;
+};
+struct Not : Expr
+{
+    public:
+        Not(Expr);
+        Expr getExpr();
+    private:
+        Expr expr;
+};
+struct And : Expr
+{
+    public:
+        And(Expr,Expr);
+        Expr getLeft();
+        Expr getRight();
+    private:
+        Expr left;
+        Expr right;
+};
+
+
