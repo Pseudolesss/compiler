450054486d1680e9fefea5034a1bb8098752951e
Julien L'hoest <j.lhoest@student.uliege.be>
2019-03-12 10:03:34 +0100

    Visitor 57.895% done

--- calc++/Visitor.cc
+++ calc++/Visitor.cc
@@ -2,9 +2,9 @@
 #include "ASTnode.hh"
 
 
-std::string Visitor::visit(ASTnode* asTnode){return "1-";}
+std::string Visitor::visit(ASTnode* asTnode){return "NOT SUPPOSED TO HAPPEN ASTNODE";}
 
-std::string Visitor::visit(Expr * expr){return "NOT SUPPOSED TO HAPPEN";}
+std::string Visitor::visit(Expr * expr){return "NOT SUPPOSED TO HAPPEN EXPRESSION";}
 
 std::string Visitor::visit(Type *type){return type->getID();}
 
@@ -16,11 +16,11 @@ std::string Visitor::visit(Formalx *formalx){return "4-";}
 
 std::string Visitor::visit(Formals *formals){return "5-";}
 
-std::string Visitor::visit(Exprx *exprx){return "6-";}
+std::string Visitor::visit(Exprx *exprx){return exprx->getExpr()->accept(this) + ", " + exprx->getExprx()->accept(this);}
 
 std::string Visitor::visit(Exprxx *exprxx){return "7-";}
 
-std::string Visitor::visit(Block *block){return "8-";}
+std::string Visitor::visit(Block *block){return "[" +  block->getExpr()->accept(this) + block->getExprx()->accept(this)+ "]";}
 
 std::string Visitor::visit(Method *method){return "9-";}
 
@@ -55,21 +55,21 @@ std::string Visitor::visit(Not *aNot){return "19-";}
 
 std::string Visitor::visit(And *anAnd){return "20-";}
 
-std::string Visitor::visit(Equal *equal){return "21-";}
+std::string Visitor::visit(Equal *equal){return "BinOp(" + "=" + ", " + equal->getLeft()->accept(this) + ", " + equal->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Lower *lower){return "22-";}
+std::string Visitor::visit(Lower *lower){return "BinOp(" + "<" + ", " + lower->getLeft()->accept(this) + ", " + lower->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(LowerEqual *lowerEqual){return "23-";}
+std::string Visitor::visit(LowerEqual *lowerEqual){return "BinOp(" + "<=" + ", " + lowerEqual->getLeft()->accept(this) + ", " + lowerEqual->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Plus *plus){return "24-";}
+std::string Visitor::visit(Plus *plus){return "BinOp(" + "+" + ", " + plus->getLeft()->accept(this) + ", " + plus->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Minus *minus){return "25-";}
+std::string Visitor::visit(Minus *minus){return "BinOp(" + "-" + ", " + minus->getLeft()->accept(this) + ", " + minus->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Times *times){return "26-";}
+std::string Visitor::visit(Times *times){return "BinOp(" + "*" + ", " + times->getLeft()->accept(this) + ", " + times->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Div *div){return "27-";}
+std::string Visitor::visit(Div *div){return "BinOp(" + "/" + ", " + div->getLeft()->accept(this) + ", " + div->getRight()->accept(this) + ")";}
 
-std::string Visitor::visit(Pow *pow){return "28-";}
+std::string Visitor::visit(Pow *pow){return "BinOp(" + "^" + ", " + pow->getLeft()->accept(this) + ", " + pow->getRight()->accept(this) + ")";}
 
 std::string Visitor::visit(Minus1 *minus1){return "29-";}
 
@@ -83,21 +83,21 @@ std::string Visitor::visit(Dot *dot){return "33-";}
 
 std::string Visitor::visit(New *aNew){return "34-";}
 
-std::string Visitor::visit(ObjID *objID){return "35-";}
+std::string Visitor::visit(ObjID *objID){return objID->getID();}
 
-std::string Visitor::visit(Literal *literal){return "36-";}
+std::string Visitor::visit(Literal *literal){return "NOT SUPPOSED TO HAPPEN LITERAL";}
 
-std::string Visitor::visit(IntLit *intLit){return "37-";}
+std::string Visitor::visit(IntLit *intLit){return std::to_string(intLit->getValue());}
 
-std::string Visitor::visit(StrLit *strLit){return "38-";}
+std::string Visitor::visit(StrLit *strLit){return strLit->getValue();}
 
-std::string Visitor::visit(BoolLit *boolLit){return "39-";}
+std::string Visitor::visit(BoolLit *boolLit){return ( boolLit->getValue() ? "true" : "false";}
 
 std::string Visitor::visit(Lpar *lpar){return "40-";}
 
 std::string Visitor::visit(Rpar *rpar){return "41-";}
 
-std::string Visitor::visit(Parenthese *parenthese){return "42-";}
+std::string Visitor::visit(Parenthese *parenthese){return parenthese->getExpr()->accept(this);}
 
 
 
